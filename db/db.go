934cc701578e132ae1e5974c432512fb9fe5dfc8
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-03-21 14:39:07 +0530

    Update file path to load .env file in db.go

--- db/db.go
+++ db/db.go
@@ -16,7 +16,7 @@ var collection *mongo.Collection
 func init() {
 	// Connect to MongoDB
 
-	err := godotenv.Load("/path/to/.env") // Update the file path to load the .env file
+	err := godotenv.Load(".env") // Update the file path to load the .env file
 	if err != nil {
 		log.Fatal("Error loading .env file:", err)
 	}
